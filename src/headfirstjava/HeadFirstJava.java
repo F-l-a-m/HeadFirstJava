881a50f1d78f978cc4a95f2b214297ec2667c5fd
F-l-a-m <>
2017-08-14 13:31:28 +0300

    Second comment

--- src/headfirstjava/HeadFirstJava.java
+++ src/headfirstjava/HeadFirstJava.java
@@ -17,6 +17,7 @@ public class HeadFirstJava {
     public static void main(String[] args) {
         // TODO code application logic here
         // test comment
+        // second comment
     }
     
 }
