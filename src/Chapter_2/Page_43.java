1fd7ec71505024fa849197dda3fcc8751d4fba30
F-l-a-m <andrew.flam@gmail.com>
2017-08-14 16:10:21 +0300

    Pages 43, 44

---
+++ src/Chapter_2/Page_43.java
@@ -0,0 +1,28 @@
+package Chapter_2;
+
+public final class Page_43 {
+
+    public static void Run() {
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
+        if (d.snare == true) {
+            d.playSnare();
+        }
+        d.playTopHat();
+    }
+}
+
+class DrumKit {
+
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+}
--- src/HeadFirstJava/HeadFirstJava.java
+++ src/HeadFirstJava/HeadFirstJava.java
@@ -2,18 +2,21 @@ package HeadFirstJava;
 
 import Chapter_1.Page_14;
 import Chapter_1.Page_20;
+import Chapter_2.Page_43;
+import Chapter_2.Page_44;
 import Chapter_3.Page_63;
 import Chapter_3.Page_64;
 
 public class HeadFirstJava {
 
     public static void main(String[] args) {
-        /*
-        Page_14.BeerSong();
-        Page_20.Run();
-        Page_63.BooksTestDrive();
-        Page_64.Islands();
-        */
+        
+        //Page_14.BeerSong();
+        //Page_20.Run();
+        //Page_43.Run();
+        Page_44.Echo();
+        //Page_63.BooksTestDrive();
+        //Page_64.Islands();
     }
 
 }
