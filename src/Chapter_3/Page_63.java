491e5539f6ee40bd409e74cc1c1c607e20a8e3af
F-l-a-m <andrew.flam@gmail.com>
2017-08-14 15:14:57 +0300

    Page 64

--- src/Chapter_3/Page_63.java
+++ src/Chapter_3/Page_63.java
@@ -1,4 +1,4 @@
-package Chapter_2;
+package Chapter_3;
 
 public class Page_63 {
 
--- src/HeadFirstJava/HeadFirstJava.java
+++ src/HeadFirstJava/HeadFirstJava.java
@@ -1,13 +1,18 @@
 package HeadFirstJava;
 
-import Chapter_2.Page_63;
+import Chapter_3.Page_63;
+import Chapter_3.Page_64;
 
 public class HeadFirstJava {
 
     public static void main(String[] args) {
-        Page_63 p63 = new Page_63();
+        /*Page_63 p63 = new Page_63();
+        System.out.println("Page 63 output:");
         p63.BooksTestDrive();
-        p63.Hobbits();
+        p63.Hobbits();*/
+        System.out.println("Page 64 output:");
+        Page_64 p64 = new Page_64();
+        p64.Islands();
     }
 
 }
