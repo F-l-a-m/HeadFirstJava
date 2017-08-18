d3fd2325ab820c3cc6fcec01dce4d8ddda59dc5b
F-l-a-m <andrew.flam@gmail.com>
2017-08-18 10:21:26 +0300

    Chapter 5, page 119

---
+++ src/Chapter_5/Page_119.java
@@ -0,0 +1,16 @@
+package Chapter_5;
+
+public final class Page_119 {
+
+    public static void CodeMagnets() {
+        for (int x = 0; x < 4; x++) {
+            for (int y = 4; y > 2; y--) {
+                System.out.println(x + " " + y);
+
+            }
+            if (x == 1) {
+                x++;
+            }
+        }
+    }
+}
--- src/HeadFirstJava/HeadFirstJava.java
+++ src/HeadFirstJava/HeadFirstJava.java
@@ -1,20 +1,15 @@
 package HeadFirstJava;
 
-import Chapter_1.Page_14;
-import Chapter_1.Page_20;
-import Chapter_2.Page_43;
-import Chapter_2.Page_44;
-import Chapter_3.Page_63;
-import Chapter_3.Page_64;
-import Chapter_3.Page_65;
-import Chapter_4.Page_88;
-import Chapter_4.Page_91;
-import Chapter_5.SimpleDotComGame;
+import Chapter_1.*;
+import Chapter_2.*;
+import Chapter_3.*;
+import Chapter_4.*;
+import Chapter_5.*;
 
 public class HeadFirstJava {
 
     public static void main(String[] args) {
-        
+
         //Page_14.BeerSong();
         //Page_20.Abcd();
         //Page_43.DrumKitTest();
@@ -26,7 +21,8 @@ public class HeadFirstJava {
         //Page_88.ClockTestDrive();
         //Page_91.Puzzle4();
         //SimpleDotComGame.SimpleDotComTestDrive();
-        SimpleDotComGame.PlayGame();
+        //SimpleDotComGame.PlayGame();
+        //Page_119.CodeMagnets();
     }
 
 }
